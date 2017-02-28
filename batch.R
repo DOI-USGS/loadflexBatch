ecb73c424581a97c4aef58d6987a17e847c73daa
Alison Appling <aappling@usgs.gov>
2017-02-27 17:09:18 -0700

    tidying comments, messages

--- batch.R
+++ batch.R
@@ -15,17 +15,18 @@ library(dplyr)
 library(loadflex)
 library(tools)
 library(rloadest)
-source('batchHelperFunctions.R') #functions stored here
+source('batchHelperFunctions.R') # functions that support this script are stored here
 
-#need at least rloadest 0.4.4 for formula fix
+# Require at least rloadest 0.4.4 for formula fix
 if(compareVersion(as.character(packageVersion("rloadest")),"0.4.4") == -1) {
   stop("rloadest version 0.4.4 or greater is required")
 }
 
+# Read the directory structure and site info file to determine what's available
 fileDF <- makeFileDF(inputs$inputFolder, constits = inputs$constituents, discharge.folder = inputs$dischargeFolder)
 allSiteInfo <- read.csv(file.path(inputs$inputFolder, inputs$siteInfo), stringsAsFactors = FALSE)
 
-#setup output directories
+# Create output directories
 nConstits <- length(inputs$constituents)
 outConstit <- file.path(rep(inputs$outputFolder, nConstits), inputs$constituents)
 sapply(outConstit, dir.create, recursive = TRUE, showWarnings = FALSE)
@@ -36,35 +37,35 @@ outTemporal <- file.path(rep(outConstit,4), c(rep("inputs", nConstits),
 sapply(outTemporal, dir.create, showWarnings = FALSE)
 
 lastConstit <- NULL
-graphics.off() #don't want open PDF connections
+graphics.off() # we don't want open PDF connections
 
 #-----------------loadflex--------------#
 
 allModels <- list() # this might get big. i'd prefer splitting & saving
 
-#loop over unique sites
+# Loop over unique site-constituent combinations, creating a set of output files
+# for each
 for(i in 1:nrow(fileDF)) {
-  message(paste('processing constituent file', fileDF$constitFile[i], '\n'))
+  message(paste0('processing constituent file ', fileDF$constitFile[i]))
   
-  #TODO: use siteInfo to get column names, fail if they aren't the same as directories
+  #TODO: if a constituent file is missing, skip it and keep going (#142)
   
-  #read in appropriate files
+  # Read in appropriate files
   siteQ <- read.csv(fileDF$qFile[i], stringsAsFactors = FALSE)
   siteConstit <- read.csv(fileDF$constitFile[i], stringsAsFactors = FALSE)
   
-  #convert date text to Dates 
-  siteConstit$date <- as.Date(siteConstit$date)
-  siteQ$date <- as.Date(siteQ$date)
+  # Convert date text to Dates 
+  siteConstit$date <- as.Date(siteConstit$date, format='%Y-%m-%d')
+  siteQ$date <- as.Date(siteQ$date, format='%Y-%m-%d')
   
-  #pull out appropriate rows of allSiteInfo for Q and constit
-  #need to extract constit and sites from file paths, so we know 
-  #what row of site info to look at
+  # Pull out appropriate rows of allSiteInfo for Q and constit. Need to extract 
+  # constit and sites from file paths so we know what row of site info to check
   constitSite <- basename(file_path_sans_ext(fileDF$constitFile[i])) 
   constitName <- basename(dirname(fileDF$constitFile[i]))
  
-  #if switching to a new consituent, open a new pdf
-  #important that fileDF is sorted by consituent!
-  #this should be the case the way makeFileDF looks at folders
+  # If switching to a new consituent, open a new pdf. It's important that fileDF
+  # is sorted by consituent! We've done this by structuring makeFileDF so it
+  # loops over folders
   if(is.null(lastConstit)) {
     pdf(height = 11, width = 8.5, 
         file = file.path(inputs$outputFolder, constitName, sprintf("%s_plots.pdf", constitName)))
@@ -77,27 +78,27 @@ for(i in 1:nrow(fileDF)) {
         file = file.path(inputs$outputFolder, constitName, sprintf("%s_plots.pdf", constitName)))
   }
   
+  # Isolate the site, constituent, and flow metadata relevant to this iteration
   constitSiteInfo <- filter(allSiteInfo, matching.site == constitSite, constituent == constitName)
   qSiteInfo <- filter(allSiteInfo, matching.site == constitSite, constituent == 'Q')
   
-  #deal with different discharge/consituent drainage areas
+  # Deal with different discharge/consituent drainage areas
   if(qSiteInfo$basin.area != constitSiteInfo$basin.area) {
     ratio <- constitSiteInfo$basin.area/qSiteInfo$basin.area
-    #modify discharge for both DFs
+    # modify discharge for both DFs
     siteConstit$Q <- siteConstit$Q*ratio
     siteQ$Q <- siteQ$Q*ratio
-    message("Scaling discharge by basin area")
+    message(sprintf(" * scaling discharge by basin area: multiplying by %1.3f", ratio))
   }
   
-  #create metadata
-  #not sure units etc are following the correct format
-  #currently depending on consistent column positions to get names
+  # Create metadata. We're depending on consistent column positions to get names
+  # and could probably do better (see #190)
   constitColName <- names(siteConstit)[3]
   qwconstitColName <- paste0(constitColName, '_qw')
   qColName <- names(siteConstit)[2]
   dateColName <- names(siteConstit)[1]
   
-  # remove duplicate observations
+  # Remove duplicate observations
   constitDupes <- table(siteConstit$date) %>% .[.>1] %>% names()
   nonFirstDupes <- unlist(lapply(constitDupes, function(cD) {
     dupes <- which(as.character(siteConstit$date) == cD)
@@ -105,8 +106,8 @@ for(i in 1:nrow(fileDF)) {
   }))
   if(length(nonFirstDupes) > 0) siteConstit <- siteConstit[-nonFirstDupes,]
   
-  # format censored data for rloadest. For ANA, Status 0 means null or blank. 
-  # Status 1 means a valid value and 2 means that the respective value is a 
+  # Format censored data for rloadest. For ANA, Status 0 means null or blank. 
+  # Status 1 means a valid value, and 2 means that the respective value is a 
   # detection limit."
   censor.statuses <- c('0'='', '1'='', '2'='<')
   siteConstit[[qwconstitColName]] <- 
@@ -118,10 +119,10 @@ for(i in 1:nrow(fileDF)) {
     siteConstit[['status']] == 0, NA, 
     ifelse(siteConstit[['status']] == 2, siteConstit[[constitColName]] / 2, # it's still bad, but use half MDL because better than full MDL
            siteConstit[[constitColName]]))
-  # remove NA values, some of which may have been added by checking status
+  # Remove NA values, some of which may have been added by checking status
   siteConstit <- siteConstit[!is.na(siteConstit[[constitColName]]), ]
   
-  # create a formal metadata object. site.id and flow.site.id must both equal
+  # Create a formal metadata object. site.id and flow.site.id must both equal
   # constitSite for our input file scheme to work
   siteMeta <- metadata(
     constituent = constitColName, consti.name = constitColName, conc.units = constitSiteInfo$units, 
@@ -131,8 +132,8 @@ for(i in 1:nrow(fileDF)) {
     flow.site.name = qSiteInfo$site.name, flow.site.id = qSiteInfo$site.id, flow.lat = qSiteInfo$lat, flow.lon = qSiteInfo$lon, flow.basin.area = qSiteInfo$basin.area
   )
   
-  # compute and save info on the site, constituent, and input datasets (we'll 
-  # recombine in the next loop). compute num.censored specially here because 
+  # Compute and save info on the site, constituent, and input datasets (we'll 
+  # recombine in the next loop). Compute num.censored specially here because 
   # we're using rloadest format for censored data (smwrQW format) and will 
   # eventually have something simpler in place for loadflex, at which point
   # we'll add that to summarizeInputs.
@@ -141,9 +142,11 @@ for(i in 1:nrow(fileDF)) {
   inputMetrics$estdat.num.censored <- NULL # assuming there isn't and shouldn't be censoring in Q. is that right?
   write.csv(inputMetrics, file.path(inputs$outputFolder, constitName, "inputs", paste0(constitSite, '.csv')), row.names=FALSE)
   
-  #fit models
+  # Fix the random number generator seed so we get the same results each time we
+  # run the script
   set.seed(9451)
-  #TODO: decide on standard column names?  user input timestep above?
+  
+  # Fit the rloadest model
   siteConstitRloadest <- setNames(
     siteConstit[c(dateColName, qwconstitColName, qColName)],
     c(dateColName, constitColName, qColName))
@@ -156,10 +159,12 @@ for(i in 1:nrow(fileDF)) {
             load.units = getInfo(siteMeta, 'load.units')), 
     site.id = getInfo(siteMeta, 'site.id'))
   
+  # Fit the interpolation model (no censoring)
   interpRect <- loadInterp(
     interp.format = "conc", interp.function = rectangularInterpolation,
     data = siteConstit, metadata = siteMeta)
   
+  # Fit the composite model (with rloadest model that doesn't do censoring)
   rloadest5forComp <- loadReg2( # only difference is the data (non-censored)
     loadReg(loadRegFormula, data = siteConstit, 
             flow = qColName, dates = dateColName, time.step = "day",
@@ -171,11 +176,11 @@ for(i in 1:nrow(fileDF)) {
     reg.model = rloadest5forComp, interp.format = "conc", interp.function = rectangularInterpolation, 
     interp.data = siteConstit)
   
-  #list of all model objects
   allModels[[constitSite]] <- list(comp = comp, interpRect = interpRect, 
                                    rloadest5param = rloadest5param)
+  # Create list of all model objects
   
-  #make predictions
+  # Make predictions
   pconc_rload <- predictSolute(rloadest5param, "conc", siteQ, se.pred = TRUE, date = TRUE)
   pconc_interp <- predictSolute(interpRect, "conc", siteQ, se.pred = TRUE, date = TRUE)
   pconc_comp <- predictSolute(comp, "conc", siteQ, se.pred = TRUE, date = TRUE)
@@ -183,12 +188,10 @@ for(i in 1:nrow(fileDF)) {
   pflux_interp <- predictSolute(interpRect, "flux", siteQ, se.pred = TRUE, date = TRUE)
   pflux_comp <- predictSolute(comp, "flux", siteQ, se.pred = TRUE, date = TRUE)
   nPreds <- nrow(siteQ)
-  allPreds <- bind_rows(pconc_rload, pconc_interp, pconc_comp)
-  allPreds$model <- rep(c('rloadest','interp','composite'), each=nPreds)
+  allConcPreds <- bind_rows(pconc_rload, pconc_interp, pconc_comp)
+  allConcPreds$model <- rep(c('rloadest','interp','composite'), each=nPreds)
     
-  #TODO: model metrics for non-rloadest models
-  #combine into DF with row for each model
-  #need to extract fitted model so rloadest functions can be used
+  # Summarize each model
   metrics <- bind_cols(
     data.frame(summarizeModel(rloadest5param)[1:2]), # site/constit info
     data.frame(REG=summarizeModel(rloadest5param)[-(1:2)]),
@@ -196,7 +199,7 @@ for(i in 1:nrow(fileDF)) {
     data.frame(CMP=summarizeModel(comp, newdata=siteQ, irregular.timesteps.ok=TRUE)[-(1:2)]))
   write.csv(x = metrics, file = file.path(inputs$outputFolder, constitName, "modelMetrics", paste0(constitSite, ".csv")), row.names = FALSE)
   
-  #make predictions
+  # Predict annual fluxes
   annualSummary <- bind_rows(
     mutate(aggregateSolute(pflux_rload, siteMeta, agg.by = "water year"), model = "REG"),
     mutate(aggregateSolute(pflux_interp, siteMeta, agg.by = "water year"), model = "INT"),
@@ -213,6 +216,7 @@ for(i in 1:nrow(fileDF)) {
     x = annualSummary, 
     file = file.path(inputs$outputFolder, constitName, "annual", paste0(constitSite, '.csv')), row.names=FALSE)
   
+  # Predict the multi-year average flux
   multiYearSummary <- bind_rows(
     mutate(aggregateSolute(pflux_rload, siteMeta, agg.by = "mean water year"), model = "REG"),
     mutate(aggregateSolute(pflux_interp, siteMeta, agg.by = "mean water year"), model = "INT"),
@@ -229,19 +233,18 @@ for(i in 1:nrow(fileDF)) {
     x = multiYearSummary, 
     file = file.path(inputs$outputFolder, constitName, "multiYear", paste0(constitSite, '.csv')), row.names=FALSE)
   
-  #plots
   writePDFreport(file = file.path(inputs$outputFolder, constitName, paste(constitSite, "report.pdf", sep = "_")),
                  intdat = siteConstit[1:4], estdat = siteQ, allPreds = allPreds, 
                  meta = siteMeta, inputCSV = inputMetrics, annualCSV = annualPreds)
-    
-  message(paste('Finished processing constituent file', fileDF$constitFile[i], '\n'))
+  # Add plots to the pdf we have open for writing
 }
 
-#close the final pdf
+# Close the final pdf
 dev.off()
 
+# Combine the outputs from each site-constituent combination into a single table
+# per output type and constituent
 allInputs <- summarizeCsvs('inputs', fileDF, inputs$outputFolder) 
 allAnnual <- summarizeCsvs('annual', fileDF, inputs$outputFolder) 
 allMultiYear <- summarizeCsvs('multiYear', fileDF, inputs$outputFolder) 
 allModelMetrics <- summarizeCsvs('modelMetrics', fileDF, inputs$outputFolder)
-
